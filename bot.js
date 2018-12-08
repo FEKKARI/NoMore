2273487ab0df1cb81d94c1b70341023533fb2b55
FEKKARI <45522149+FEKKARI@users.noreply.github.com>
2018-12-08 20:36:16 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -186,7 +186,7 @@ client.on('guildMemberAdd', member => {
     invites[member.guild.id] = guildInvites;
     const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
     const inviter = client.users.get(invite.inviter.id);
-    const logChannel = member.guild.channels.find(channel => channel.name === "اسم روم الولكم");
+    const logChannel = member.guild.channels.find(channel => channel.name === "welcome");
     logChannel.send(${member} Invited by: <@${inviter.id}>);
   });
 });
